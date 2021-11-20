3e42dd9252be74b5352efe0de4daea6bf99a3f68
HemalathaBommireddy <hbommireddy@innominds.com>
2021-11-20 21:11:47 +0530

    Classes for SignUp

---
+++ source/repos/stackoverflow/SignUp/SignUpPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SignUp
+{
+    public class SignUpPage
+    {  
+        public string UserName { get; set;}
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string ConformPassWord { get; set;}
+    }
+}
