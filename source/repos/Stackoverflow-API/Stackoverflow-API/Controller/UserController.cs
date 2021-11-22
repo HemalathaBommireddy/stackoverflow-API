00e1d989a10b8329fa25e6d88857e1f426aad2e3
HemalathaBommireddy <hbommireddy@innominds.com>
2021-11-22 21:46:49 +0530

    Registration page created successfully

---
+++ source/repos/Stackoverflow-API/Stackoverflow-API/Controller/UserController.cs
@@ -0,0 +1,58 @@
+﻿using MethodsforAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAPIModel;
+
+namespace Stackoverflow_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ISignUpClass _SignUpClass;
+        public UserController(ISignUpClass signupclass)
+        {
+            _SignUpClass = signupclass;
+        }
+        [HttpPost, Route("api/Register")]
+        public bool Register(UserDetailsRegister _UserDetailsRegister)
+        {
+            if (_SignUpClass.InsertUserDetailsIntoSignUpForm(_UserDetailsRegister))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        
+        [HttpGet, Route("api/UserDetailsRegister/UserLogin/{_EmailId}/{_Password}")]
+        public IEnumerable<UserDetailsRegister> UserSignUp(string _EmailId, string _Password)
+        {
+
+            return _SignUpClass.UserLogin(_EmailId, _Password);
+
+
+
+        }
+        [HttpPut, Route("api/UserDetailsRegister/ForgetPassword/{_Password}")]
+        public bool ForgetPassword(string _Password)
+        {
+
+            return _SignUpClass.ForgetPassword(_Password);
+
+
+
+        }
+
+    }
+}
+    
+
